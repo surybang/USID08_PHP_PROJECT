6be1aba7b93c913907c74865a17c488792b4423a
surybang <44811505+surybang@users.noreply.github.com>
2022-12-10 13:38:06 +0100

    update php mail
    last version + anonymized addresses

--- php_mail.php
+++ php_mail.php
@@ -1,18 +1,40 @@
 
 <?php 
-#ini_set("display_errors", 1);
+ini_set("display_errors", 1);
 # utilisation de sendmail ; 
-# https://www.youtube.com/watch?v=Fywr8gIVdLY
-#TODO : automatiser + traitement erreurs 
-$to = "test@outlook.fr";
-$subject = "test mail php" ;
-$message = "Arrête de me répondre avec un 'non'";
-$headers = "Content-type : text/plain ; charset = utf8 \r\n";
-$headers .= "From: hos.fabien@outlook.fr \r\n"; 
+# https://www.youtube.com/watch?v=Fywr8gIVdLY ;
 
-if(mail($to, $subject, $message,$headers))
-	echo 'envoye';
-else 
-	echo'erreur';
+
+
+<?php 
+ini_set("display_errors", 1);
+# utilisation de sendmail ; 
+# https://www.youtube.com/watch?v=Fywr8gIVdLY ;
+
+
+
+function mailSend($adr,$reco){
+    if (isset($adr))
+    {
+        $to = $adr;
+        $subject = "Vos recommandations suite au questionnaire" ;
+        $message = "Bonjour ".$adr." , <br> Vous avez répondu à notre questionnaire concernant votre prochaine destination en France, nous vous proposons de le découvrir en vous rendant sur notre site web dédié <a href='https://projet-medas-destination-france.000webhostapp.com' target = '_blank'> https://projet-medas-destination-france.000webhostapp.com </a> <br> Bien cordialement, <br> YOUR NAME" ;
+        $headers = "Content-type : text/plain ; charset = utf8 \r\n";
+        $headers .= "From: YOUR NAME <YOUR@ADDRESS.com> \r\n"; 
+        if(mail($to, $subject, $message,$headers))
+        {
+            echo 'envoye';
+        }else
+        {
+        echo'erreur';
+        }
+    }
+
+}
+
+
+
+?>
+
 
 ?>
